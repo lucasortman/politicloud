e4f72e6b9ccf69f5bcf2299e4733ebbf3ee4b618
lucasortman <127875317+lucasortman@users.noreply.github.com>
2025-01-13 15:42:50 -0300

    New version
    switched txts for json files, code rewritten

--- politicloud/candidato.py
+++ politicloud/candidato.py
@@ -1,22 +1,32 @@
 import matplotlib.pyplot as plt
+import json
 from wordcloud import WordCloud
 
 
-# armazena as stopwords em uma lista
-stopwords = open('./resources/stopwords.txt', encoding='UTF8').read()
-lista_stopwords = stopwords.split()
-
-# define a classe candidato e suas características, além de abrir o arquivo onde os tweets ficam armazenados
+# inicia a classe Candidato, recebendo alguns valores manuais e carregando o json para receber os valores da fonte (Twitter)
 class Candidato():
-    def __init__(self, nome:str, twitter:str, idade:int, partido:str, escolaridade:str, profissao:str, bio:str, tweets) -> None:
-        self.nome = nome
-        self.twitter = twitter
+    def __init__(self, idade:int, partido:str, escolaridade:str, profissao:str, source:str) -> None:
         self.idade = idade
         self.partido = partido
         self.escolaridade = escolaridade
         self.profissao = profissao
-        self.bio = bio
-        self.tweets = open(tweets, encoding='UTF8').read()
+        self.source = source
+        with open(source, 'r', encoding='UTF8') as file:
+            data = json.load(file)
+        self.nome = data['user']['name']
+        self.twitter = '@' + data['user']['screen_name']
+        self.bio = data['user']['description']
+        self.tweets = {}
+
+
+# carrega os tweets do arquivo para dentro do objeto Candidato na forma de um dicionário, usando os campos "id" e "full_text" do arquivo json
+    def load_tweets(self):
+        with open(self.source, 'r', encoding='UTF8') as file:
+            data = json.load(file)
+            
+        for tweet in data['tweets']:
+            self.tweets.update({tweet['id']: tweet['full_text']})
+
 
 # apresenta um resumo do candidato
     def status(self):
@@ -28,20 +38,29 @@ class Candidato():
             f"{self.bio}\n"
         )
 
-# adiciona o nome do candidato e seu nome de usuário do twitter (com e sem @) na lista de stopwords
+
+# cria a lista de stopwords (termos a serem ignorados) e adiciona a ela o nome do próprio candidato e o seu próprio nome de usuário (com e sem @)
     def add_stopwords(self):
+        stopwords = open('./resources/stopwords.txt', encoding='UTF8').read()
+        lista_stopwords = stopwords.split()
         lista2 = [self.nome, self.twitter, self.twitter.lstrip('@')] + self.nome.split()
         nova_lista = lista_stopwords + lista2
         return nova_lista
 
+
 # cria a wordcloud do objeto, que abrirá como uma imagem estática de fundo preto e letras coloridas
     def create_wordcloud(self):
+        self.load_tweets()
+        raw_txt = ''
+        for tweet_txt in self.tweets.values():
+            raw_txt += tweet_txt
+        
         wordcloud = WordCloud(
             stopwords=self.add_stopwords(),
             width=700,
             height=700,
         )
-        wordcloud.generate(self.tweets)
+        wordcloud.generate(raw_txt)
         plt.figure(figsize=(10,10))
         plt.imshow(wordcloud, interpolation='bilinear')
         plt.axis('off')
